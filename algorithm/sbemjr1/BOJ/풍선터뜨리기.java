7d91e5ebaa1126a348b22e04a507f52604674ed0
이승준 <sbemjr1@naver.com>
2024-02-06 14:24:54 +0900

    0205 풍선터뜨리기,회전하는큐 스터디 숙제

--- algorithm/sbemjr1/BOJ/풍선터뜨리기.java
+++ algorithm/sbemjr1/BOJ/풍선터뜨리기.java
@@ -1,14 +1,17 @@
 package sbemjr1.BOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 class qArr {
 	int idx;
 	int val;
 	
-	qArr(int idx, int val) {
+	public qArr(int idx, int val) {
         this.idx = idx;
         this.val = val;
     }
@@ -18,44 +21,34 @@ public class 풍선터뜨리기 {
 	static int N, K, cnt, v, arr[];
 	static qArr tmp;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Queue<qArr> q = new ArrayDeque<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Deque<qArr> dq = new ArrayDeque<>();
 		
-		N = sc.nextInt();
+		N = Integer.parseInt(br.readLine());
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			q.offer(new qArr(i,sc.nextInt()));
+			dq.add(new qArr(i, Integer.parseInt(st.nextToken())));
 		}
 		
-		tmp = q.poll();
-		v = tmp.val;
-		System.out.print(tmp.idx+" ");
-		
-		while(!q.isEmpty()) {
-			tmp = q.poll();
-			cnt++;
-			if (cnt == v) {
-				v = tmp.val;
-				if (v < 1) {
-					v += q.size();
-				} else if (v >= q.size()) {
-					v -= q.size();
+		while(!dq.isEmpty()) {
+			qArr b = dq.removeFirst();
+			System.out.print(b.idx+" ");
+			if(!dq.isEmpty()) {
+				if(b.val >= 0) {
+					for (int j = 0; j < b.val - 1; j++) {
+						dq.add(dq.removeFirst());
+					}
 				}
-				System.out.print(tmp.idx + " ");
-				cnt=0;
-			} else {
-				q.offer(tmp);
+				else {
+					for (int j = 0; j < -(b.val); j++) {
+						dq.addFirst(dq.removeLast());
+					}
+				}
 			}
 		}
+		
 	}
 
 }
-
-//
-//# 입력
-//10
-//1 -2 3 -4 5 -6 7 -8 9 -10
-//
-//# 출력
-//1 2 9 3 6 5 7 8 10 4
