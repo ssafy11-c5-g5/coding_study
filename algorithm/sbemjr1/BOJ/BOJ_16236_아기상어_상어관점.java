a800299a2b9526bacee90656befac97bf840e1ae
sbemjr1 <sbemjr1@naver.com>
2024-03-07 09:45:05 +0900

    0307 아기상어_상어관점으로 수정

--- algorithm/sbemjr1/BOJ/BOJ_16236_아기상어_상어관점.java
+++ algorithm/sbemjr1/BOJ/BOJ_16236_아기상어_상어관점.java
@@ -9,7 +9,7 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class BOJ_16236_아기상어 {
+public class BOJ_16236_아기상어_상어관점 {
 	static class Shark {
 		int r,c,weight;
 
@@ -58,9 +58,10 @@ public class BOJ_16236_아기상어 {
 		}
 	}
 	
-	static int N, map[][], ans, dist, l;
+	static int N, map[][], ans, dist, cnt;
 	static Shark babyShark;
 	
+	static PriorityQueue<Target> pq;
 	static boolean[][] v;
 	
 	static int[] dr = {-1,1,0,0};
@@ -77,7 +78,7 @@ public class BOJ_16236_아기상어 {
 			st = new StringTokenizer(br.readLine());
 			for (int c = 0; c < N; c++) {
 				map[r][c] = Integer.parseInt(st.nextToken());
-				// 상어 위치 찾기(초기 상어 크기 2)
+				// 초기 상어 위치 찾기(초기 상어 크기 2)
 				if(map[r][c] == 9) {
 					babyShark = new Shark(r,c,2);
 					map[r][c] = 0;
@@ -91,50 +92,33 @@ public class BOJ_16236_아기상어 {
 		//먹을 수 있는지? 본인 몸집보다 작은 경우 - 먹기 가능
 		//비교 조건
 		//1.가까운 것 2.위에 있는 것 3.왼쪽에 있는 것
-		PriorityQueue<Target> pq = new PriorityQueue<>();
+		pq = new PriorityQueue<>();
 		
-		//몸집 키우기 (자기 몸무게 만큼 먹으면 1성장)
-		int cnt = 0;
 		while(true) {
 			pq.clear();
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++) {
-					if(map[r][c] > 0 && babyShark.weight > map[r][c]) { // 먹을 수 있거나 물고기가 있을 때
-						v = new boolean[N][N];
-						dist = 0;
-						if(bfs(r,c)) { // 최단 거리 확인 및 갈 수 있는지 확인(물고기 -> 상어)
-							pq.offer(new Target(r,c,dist));
-						}
-					}
-				}
-			}
-//			System.out.println(pq);
-//			System.out.println(pq.peek());
-			if(pq.isEmpty()) { // 갈 수 있는 곳이 없는 경우
+			v = new boolean[N][N];
+			bfs(babyShark.r,babyShark.c);
+			
+			if(pq.isEmpty()) {
 				break;
 			}
 			Target result = pq.poll();
-			cnt++;//먹은 물고기 마리수
-			ans += result.distance; //결과물
-			//상어 위치 갱신
+			ans += result.distance;
+			cnt++;
+			
 			babyShark.r = result.r;
 			babyShark.c = result.c;
-//			babyShark.weight = result.distance;
-			//자기 몸무게만큼 먹었으면 1 성장
+			
 			if(cnt == babyShark.weight) {
 				babyShark.weight++;
 				cnt = 0;
 			}
-//			System.out.println(babyShark.weight);
-			map[result.r][result.c] = 0;
-//			print();
-//			sc.nextLine();
+			map[babyShark.r][babyShark.c] = 0;
 		}
-		
 		System.out.println(ans);
 	}
 
-	private static boolean bfs(int r, int c) {
+	private static void bfs(int r, int c) {
 		Queue<Fish> q = new ArrayDeque<>();
 		q.offer(new Fish(r,c,0));
 		v[r][c] = true;
@@ -144,14 +128,9 @@ public class BOJ_16236_아기상어 {
 			int nowR = f.r;
 			int nowC = f.c;
 			int nowL = f.length;
-//			System.out.println(nowR+" "+nowC);
-//			System.out.println(babyShark.r);
-//			System.out.println(babyShark.c);
 			
-			if(nowR == babyShark.r && nowC == babyShark.c) {
-				dist = nowL;
-//				System.out.println(nowL);
-				return true;
+			if(map[nowR][nowC] > 0 && map[nowR][nowC] < babyShark.weight) {
+				pq.offer(new Target(nowR,nowC,nowL));
 			}
 			
 			for (int d = 0; d < 4; d++) {
@@ -172,6 +151,5 @@ public class BOJ_16236_아기상어 {
 				q.offer(new Fish(nextR,nextC,nextL));
 			}
 		}
-		return false;
 	}
 }
